d38c3d6b5f44324bb3c5230b5cc174451d55ef5b
Boris Gromov <bugromov@ya.ru>
2022-01-27 13:00:33 +0300

    Secured key deleted

--- Fitathlon_Portal/settings.py
+++ Fitathlon_Portal/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(m5exo5y$44+8a-97($sz^!uwd#jk9(u-zc$4bek3x5^=q0432'
+SECRET_KEY = ''
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
